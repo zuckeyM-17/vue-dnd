23eb90d42ea52981f6209e88491df1d7e62fe08a
松村和輝 <k.matsumura0314@gmail.com>
2018-03-02 18:30:10 +0900

    環境を整備

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const projectRoot = path.resolve(__dirname, './')
+
+module.exports = {
+  watch: true,
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            js: 'babel-loader',
+            options: {
+              presets: ["es2015", "stage-3"]
+            }
+          }
+        }
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    modules: [
+        "node_modules"
+    ],
+    alias: {
+        vue: 'vue/dist/vue.common.js'
+    }
+  },
+};
